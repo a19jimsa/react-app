a775d86cca8552d29b5929d9e8e10d20545d534c
a19jimsa <a19jimsa@student.his.se>
2022-02-18 18:03:08 +0100

    do

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/dontainer";
 import Menu from "./components/benu";
-import Header from "./components/header";
 
-ReactDOM.render(<Header />, document.getElementById("header"));
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
