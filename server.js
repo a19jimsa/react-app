ae3187a8721ce37c73da826560aa9ed4c66c9f6e
a19jimsa <a19jimsa@student.his.se>
2022-02-18 17:44:56 +0100

    Added build to deploy

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ require("dotenv").config({ path: ".env" });
 const port = process.env.PORT || 5000;
 app.use(express.json());
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("./server/db/conn");
 
 const path = require("path");
 
-app.use(express.static('../src'));
-app.use(express.static('../public'));
-app.use(express.static('../server'));
-
 //Include routes
-const commentRoutes = require("./routes/commentRoutes")
+const commentRoutes = require("./server/routes/commentRoutes")
 app.use("/comments", commentRoutes);
 
-const threadRoute = require("./routes/threadRoutes")
+const threadRoute = require("./server/routes/threadRoutes")
 app.use("/threads", threadRoute);
 
-const userRoute = require("./routes/userRoutes")
+const userRoute = require("./server/routes/userRoutes")
 app.use("/users", userRoute);
 
-app.use(express.static(path.resolve(__dirname, "../src/build")));
+app.use(express.static('build'));
+app.get("*", (req, res) => {
+  req.sendFile(path.resolve(__dirname, 'build', "index.html"));
+});
  
 app.listen(port, () => {
   // perform a database connection when server starts
