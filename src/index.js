e00aa77d3e68276a0ed747783fa1f1a5d0d6fc3d
a19jimsa <a19jimsa@student.his.se>
2022-02-18 12:55:28 +0100

    grger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/container";
-import Menu from "./components/menu";
+import Menu from "./components/benu";
 
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
