5ac88d7c26edaa46636dffd0ae472138164dd7ec
a19jimsa <a19jimsa@student.his.se>
2022-02-18 18:14:18 +0100

    Finally finished for build to heroku

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/dontainer";
 import Menu from "./components/benu";
-import Header from "./components/header";
+import Header from "./components/beader";
 
-ReactDOM.render(<Header />, document.getElementById("header"));
+ReactDOM.render(<Header />, document.getElementById("head"));
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/dontainer";
 import Menu from "./components/benu";
+import Header from "./components/beader";
 
+ReactDOM.render(<Header />, document.getElementById("head"));
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
