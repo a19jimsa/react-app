0e1209ca1c65b26aca691603593510e3da28e39b
a19jimsa <a19jimsa@student.his.se>
2022-02-18 12:35:59 +0100

    changed names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Container from "./components/Container";
-import Menu from "./components/Menu";
+import Container from "./components/container";
+import Menu from "./components/menu";
 
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
