9259836f8161e9c303e273edf4317071030a41bb
a19jimsa <a19jimsa@student.his.se>
2022-02-18 12:20:30 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/Container";
 import Menu from "./components/Menu";
-import Header from "./components/Header";
 
 ReactDOM.render(<Container />, document.getElementById("content"));
 ReactDOM.render(<Menu />, document.getElementById("menu"));
-ReactDOM.render(<Header />, document.getElementById("head"));
