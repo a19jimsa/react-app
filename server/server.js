b6ccc3f769e3462fc7be09128e6ac14d42e27242
a19jimsa <a19jimsa@student.his.se>
2022-02-18 13:17:24 +0100

    Changed stuff

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: ".env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // get driver connection
 const dbo = require("./db/conn");
+const path = require('path');
 
 app.use(express.static('../src/components'));
 app.use(express.static('../public'));
 app.use(express.static('../server'));
+
+app.use('/', express.static(path.join(__dirname, 'src')));
 
 //Include routes
 const commentRoutes = require("./routes/commentRoutes")
