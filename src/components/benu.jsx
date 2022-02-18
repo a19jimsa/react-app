72eb00ff097a97a07c321ac0a4926259649e6194
a19jimsa <a19jimsa@student.his.se>
2022-02-18 12:57:04 +0100

    Added changes

--- src/components/benu.jsx
+++ src/components/benu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Container from "./container";
+import Container from "./dontainer";
 import {
   AwesomeButton
 } from 'react-awesome-button';
