f4021b3d866bc528f30b6cd78bcb7149d11ba796
a19jimsa <a19jimsa@student.his.se>
2023-07-05 21:40:02 +0200

    Test

--- server.js
+++ server.js
@@ -9,25 +9,24 @@ const dbo = require("./server/db/conn");
 const path = require("path");
 
 //Include routes
-const commentRoutes = require("./server/routes/commentRoutes")
+const commentRoutes = require("./server/routes/commentRoutes");
 app.use("/comments", commentRoutes);
 
-const threadRoute = require("./server/routes/threadRoutes")
+const threadRoute = require("./server/routes/threadRoutes");
 app.use("/threads", threadRoute);
 
-const userRoute = require("./server/routes/userRoutes")
+const userRoute = require("./server/routes/userRoutes");
 app.use("/users", userRoute);
 
-app.use(express.static('build'));
+app.use(express.static("build"));
 app.get("*", (req, res) => {
-  req.sendFile(path.resolve(__dirname, 'build', "index.html"));
+  req.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
-  console.log(`Server is running on porten: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,130 +1,174 @@
 import React from "react";
 import Breadcrums from "./breadcrums";
 import Quote from "./quote";
-import { Facebook } from 'react-content-loader'
+import { Facebook } from "react-content-loader";
 
-const MyFacebookLoader = () => <Facebook />
+const MyFacebookLoader = () => <Facebook />;
 
 class Post extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {data: [], show: false, placeholder: "Skriv ett svar", content: "", loaded: false, breadcrum: this.props.breadcrum};
-        this.handleClick = this.handleClick.bind(this);
-        this.handleShowClick = this.handleShowClick.bind(this);
-        this.handleOnChange = this.handleOnChange.bind(this);
-        this.handleOnChangeSearch = this.handleOnChangeSearch.bind(this);
-        this.handleLikeClick = this.handleLikeClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      show: false,
+      placeholder: "Skriv ett svar",
+      content: "",
+      loaded: false,
+      breadcrum: this.props.breadcrum,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleShowClick = this.handleShowClick.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnChangeSearch = this.handleOnChangeSearch.bind(this);
+    this.handleLikeClick = this.handleLikeClick.bind(this);
+  }
 
-    async componentDidMount(){
-        //GET all comments on specific thread
-        await fetch("/comments/" + this.props.id, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json' }
+  async componentDidMount() {
+    //GET all comments on specific thread
+    await fetch("/comments/" + this.props.id, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data, loaded: true });
+      });
+  }
+
+  async createAnswer() {
+    const date = new Date();
+    const datetime =
+      date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    const answer = {
+      id: this.props.id,
+      content: this.state.content,
+      posted: datetime,
+      user: "Jimmy",
+    };
+
+    await fetch("/comments/" + this.props.id, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(answer),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.componentDidMount();
+        //Update thread with latest post
+        fetch("/threads/updatethread/" + this.props.id, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(answer),
         })
-            .then((response) => response.json()).then(data => {
-                this.setState({data: data, loaded: true});
-        });
+          .then((response) => response.json())
+          .then((data) => {
+            this.setState({ content: "" });
+          });
+      });
+  }
+
+  handleShowClick(message) {
+    if (message !== "") {
+      message = "[quote]" + message + "[/quote]";
     }
 
-    async createAnswer(){
-        const date = new Date();
-        const datetime = date.toLocaleDateString() + " " + date.toLocaleTimeString();
-        const answer = {
-            "id": this.props.id,
-            "content": this.state.content,
-            "posted": datetime,
-            "user": "Jimmy"
-        }
+    this.setState((prevState) => ({
+      show: !prevState.show,
+      content: message,
+    }));
+  }
 
-        await fetch("/comments/"+this.props.id, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json' },
-            body: JSON.stringify(answer)
-        }).then((response) => response.json()).then(data => {
-            this.componentDidMount();
-            //Update thread with latest post
-            fetch("/threads/updatethread/" + this.props.id, {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json' },
-                body: JSON.stringify(answer)
-                }).then((response) => response.json()).then(data => {
-                    this.setState({content: ""})
-            });
-        });
-    }
+  handleClick() {
+    this.handleShowClick();
+    this.createAnswer();
+  }
 
-    handleShowClick(message){
-        if(message !== ""){
-            message = "[quote]"+message+"[/quote]";
-        }
-        
-        this.setState(prevState => ({
-            show: !prevState.show,
-            content: message
-        }))
-    }
+  async updateState(query) {
+    await fetch("/comments/" + this.props.id + "/" + query, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+      });
+  }
 
-    handleClick(){
-        this.handleShowClick();
-        this.createAnswer();
-    }
+  handleOnChangeSearch(event) {
+    this.updateState(event.target.value);
+  }
 
-    async updateState(query){
-        await fetch("/comments/"+this.props.id+"/"+query, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json' }
-        })
-            .then((response) => response.json()).then(data => {
-                this.setState({data: data})
-        });
-    }
+  handleOnChange(event) {
+    this.setState({ content: event.target.value });
+  }
 
-    handleOnChangeSearch(event){
-        this.updateState(event.target.value);
-    }
+  async handleLikeClick(id) {
+    await fetch("/comments/like/" + id, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.componentDidMount();
+      });
+  }
 
-    handleOnChange(event){
-        this.setState({content: event.target.value});
-    }
-
-    async handleLikeClick(id){
-        await fetch("/comments/like/"+id, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'}
-        }).then((response) => response.json()).then(data => {
-            this.componentDidMount();
-        })
-    }
-
-    render() { 
-        if(this.state.loaded){
-        return <div>
-            <div className="threadhead"><Breadcrums value={this.state.breadcrum} /><div>Sök i tråd: <input type="text"onChange={this.handleOnChangeSearch}></input></div></div>
-            {this.state.data.map(tag=><div key={tag._id} className="post">
-                <h1>{tag.topic}</h1>
-                <div className="postHead">Datum: {tag.posted}</div>
-                <div className="postContent">
-                    <div className="postInfo">Användare: {tag.user}</div>
-                    <div className="postComment">
-                        <div className="postMessage"><Quote content={tag.content} /></div>
-                        <div className="quoterow">
-                            <button onClick={this.handleShowClick.bind(this, tag.content)}>Citera</button>
-                            <button onClick={this.handleLikeClick.bind(this, tag._id)}>Gilla({tag.likes})</button>
-                        </div>
-                    </div>
+  render() {
+    //if(this.state.loaded){
+    return (
+      <div>
+        <div className="threadhead">
+          <Breadcrums value={this.state.breadcrum} />
+          <div>
+            Sök i tråd:{" "}
+            <input type="text" onChange={this.handleOnChangeSearch}></input>
+          </div>
+        </div>
+        {this.state.data.map((tag) => (
+          <div key={tag._id} className="post">
+            <h1>{tag.topic}</h1>
+            <div className="postHead">Datum: {tag.posted}</div>
+            <div className="postContent">
+              <div className="postInfo">Användare: {tag.user}</div>
+              <div className="postComment">
+                <div className="postMessage">
+                  <Quote content={tag.content} />
                 </div>
-            </div>)}
-            {this.state.show ? <div className="createPost">
-            <textarea onChange={this.handleOnChange} placeholder={this.state.placeholder} value={this.state.content}></textarea>
+                <div className="quoterow">
+                  <button
+                    onClick={this.handleShowClick.bind(this, tag.content)}
+                  >
+                    Citera
+                  </button>
+                  <button onClick={this.handleLikeClick.bind(this, tag._id)}>
+                    Gilla({tag.likes})
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        {this.state.show ? (
+          <div className="createPost">
+            <textarea
+              onChange={this.handleOnChange}
+              placeholder={this.state.placeholder}
+              value={this.state.content}
+            ></textarea>
             <button onClick={this.handleClick}>Skicka svar</button>
             <button onClick={this.handleShowClick}>Ångra</button>
-        </div> : <button onClick={this.handleShowClick.bind(this, "")}>Skriv Svar</button>}
-        </div>
-        }else{
+          </div>
+        ) : (
+          <button onClick={this.handleShowClick.bind(this, "")}>
+            Skriv Svar
+          </button>
+        )}
+      </div>
+    );
+    /*}else{
             return <div><MyFacebookLoader /></div>
-        }
-    }
+        }*/
+  }
 }
 
 export default Post;
